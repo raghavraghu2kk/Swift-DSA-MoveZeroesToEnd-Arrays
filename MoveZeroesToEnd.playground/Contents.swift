aff073e3154fe772ccbfbb6b74548aa36aaf6279
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-04-03 05:22:52 +0530

    Move Zeros to End

--- MoveZeroesToEnd.playground/Contents.swift
+++ MoveZeroesToEnd.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func moveZeroesToEnd(_ arr: inout [Int]) -> [Int]{
+    var j : Int = -1
+    for i in 0..<arr.count {
+        if(arr[i] == 0){
+            j = i
+            break
+        }
+    }
+    if j == -1 {
+        return arr
+    }
+    for i in j+1..<arr.count {
+        if(arr[i] != 0){
+            (arr[i],arr[j]) = (arr[j],arr[i])
+            j += 1
+        }
+    }
+    
+    return arr
+}
+
+func moveZeroes(_ nums: inout [Int]) {
+        var j : Int = 0
+        for i in 0..<nums.count {
+            if(nums[i] != 0){
+                nums.swapAt(i,j)
+                j += 1
+            }
+        }
+    }
+
+var arr = [0,1,0,3,12]
+print(moveZeroes(&arr))
+print(arr)
+moveZeroesToEnd(&arr)
+print(arr)
